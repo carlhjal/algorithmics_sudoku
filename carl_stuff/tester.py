4284c674247e7b8bf67fa1b8f48fa87357a76ea8
carlhjal <78918181+carlhjal@users.noreply.github.com>
2024-01-19 17:43:21 +0200

    base logic

--- carl_stuff/tester.py
+++ carl_stuff/tester.py
@@ -2,12 +2,22 @@ import numpy as np
 import module.board_printer as bp
 import module.gen_random_board as gen
 
+# quite like this logic
 # https://medium.com/codex/building-a-simple-sudoku-solver-in-python-with-numpy-1a8ea6f5bff5
 
-def get_quadrant():
-    pass
+get get_quadrant(board, row, column):
+
+
+def get_possible(board, row, column):
+    
 
 def solver(board):
+    
+    for row in board:
+        for column in row:
+            possible_val = get_possible(board, row, column)
+            board[row, column]
+    
 
     
 
@@ -15,7 +25,8 @@ def main():
     empty_board = np.zeros((9,9), dtype=np.uint8)
     sudoku = gen.gen(empty_board, 17)
     bp.print_board(sudoku)
-    solved_sudoku = solver(sudoku)
+    print(sudoku[0, 0])
+    #solved_sudoku = solver(sudoku)
     bp.print_board(solved_sudoku)
 
 if __name__ == "__main__":
