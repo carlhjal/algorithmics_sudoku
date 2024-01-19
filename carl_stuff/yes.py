23c5e063fa6e83b0de499d180fece29417b44483
carlhjal <78918181+carlhjal@users.noreply.github.com>
2024-01-19 13:00:51 +0200

    board printing

--- carl_stuff/yes.py
+++ carl_stuff/yes.py
@@ -1,1 +1,25 @@
-print("hello")
+import numpy as np
+
+def print_board(board):
+    """
+    Prints the board
+    """
+    sep = "-"*25
+
+    for i in range(9):
+    
+        if not i % 3:
+            print(sep)
+
+        row = (str(board[i]).strip("[]"))
+
+        print("|" + " " + row[:6] + "|" + " " +  row[6:12] + "|" + " " + row[12:] + " " + "|")
+
+    print(sep)
+
+def main():
+    empty_board = np.zeros((9,9), dtype=np.uint8)
+    print_board(empty_board)
+
+if __name__ == "__main__":
+    main()
