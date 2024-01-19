0d9271063283d35eb3e1db04f8beb8c43bbd4767
carlhjal <78918181+carlhjal@users.noreply.github.com>
2024-01-19 14:44:25 +0200

    someting

--- carl_stuff/modules/gen_random_board.py
+++ carl_stuff/modules/gen_random_board.py
@@ -12,7 +12,7 @@ import print_board
 def gen(empty_board, nums_keep):
     """
     Very stupid generator but makes a valid sudoku and numbers can just be removed to make it harder
-    nums_keep is how many numbers should remain in the sudoku, has to be more than 17
+    nums_keep is how many numbers should remain in the sudoku, has to be at least 17
     """
 
     assert nums_keep > 16
@@ -25,7 +25,7 @@ def gen(empty_board, nums_keep):
         empty_board[i] = np.roll(num_arr, 3*i)
     
     num_els = empty_board.shape[0] * empty_board.shape[1]
-    indices = [i for i in range(num_els)]
+    indices = list(range(num_els))
 
     for i in range(num_els - nums_keep):
         zero_ind = random.choice(indices)
