488ba258278bf7e69d49b8e7a27db728f40c73a9
carlhjal <78918181+carlhjal@users.noreply.github.com>
2024-01-19 16:38:14 +0200

    rename

--- carl_stuff/tester.py
+++ carl_stuff/tester.py
@@ -1,9 +1,11 @@
 import numpy as np
+from modules import gen_random_board
 from modules import print_board
 
 def main():
     empty_board = np.zeros((9,9), dtype=np.uint8)
-    print_board.print_board(empty_board)
+    sudoku = gen_random_board.gen(empty_board, 17)
+    print_board.print_board(sudoku)
 
 if __name__ == "__main__":
     main()
